eaae3fdaf47e5e95906f375cb812b9f9fa118922
Maua Imani <imanimaua@gmail.com>
2025-11-28 01:25:38 +0100

    Added schedule router

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.core.talents.routes import talents
+from app.core.schedule.routes import schedule
 from app.core.constraints.talent_constraints.routes import talent_constraints
 from app.core.constraints.constraint_rules.routes import constraint_rules
 from app.core.shift_template.routes import shift_templates
@@ -10,12 +11,13 @@ from app.authentication.routes import auth_router
 app = FastAPI(title="Shiftly", version="1.0")
 
 
+app.include_router(auth_router, prefix="/users")
 app.include_router(talents, prefix="/talents")
 app.include_router(talent_constraints, prefix="/talent_constraints")
 app.include_router(constraint_rules, prefix="/constraint_rules")
 app.include_router(shift_period, prefix="/shift_period")
 app.include_router(shift_templates, prefix="/shift_templates")
-app.include_router(auth_router, prefix="/users")
+app.include_router(schedule, prefix="/schedule")
 
 
 
