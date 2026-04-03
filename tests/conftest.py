fd38a30455edceb206c3d7f03a9b656a082c64eb
Maua Elaine Imani <elainewalter7022@gmail.com>
2026-04-03 13:46:07 +0200

    created shared fixtures for tests

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+import pytest
+from datetime import datetime
+from app.core.schedule.shifts.schema import shiftSpecification
+from app.core.schedule.allocator.entities import assignment
+from app.core.utils.enums import Role
+from app.core.schedule.talents.schema import talentAvailability
+
+@pytest.fixture
+def make_shift():
+    def _factory(start_time: datetime, end_time:datetime):
+        return shiftSpecification(
+            template_id=1,
+            start_time=start_time,
+            end_time=end_time,
+            shift_name="am",
+            role_name= Role.SERVER,
+            role_count=1
+        )
+
+    return _factory
+
+
+@pytest.fixture
+def make_asignment():
+    def _factory(talent_id: int, shift_id: int, shift: shiftSpecification):
+        return assignment(
+            talent_id=talent_id,
+            shift_id=shift_id,
+            shift=shift
+        )
+    return _factory
+
+
+@pytest.fixture
+def make_context():
+    def _factory(talent_id: int, shift: shiftSpecification, assignments: list[assignment], availability:talentAvailability = None):
+        return {
+            "talent_id": talent_id,
+            "shift": shift,
+            "availability": availability,
+            "assignments": assignments
+        }
+    
+    return _factory
+
