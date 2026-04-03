0c1c82017a6db42e77f7b5ce2c6e39c10329d5c6
Maua Elaine Imani <elainewalter7022@gmail.com>
2026-04-03 21:19:10 +0200

    Made slight changes to the utility function

--- app/core/schedule/allocator/engine/utils.py
+++ app/core/schedule/allocator/engine/utils.py
@@ -5,9 +5,9 @@ def get_break_duration(shift_name:str):
 
     shift_name = shift_name.lower()
     break_duration = {
-        Shifts.AM.value: 0.75,
+        Shifts.AM.value: 0.5,
         Shifts.PM.value: 0.5,
         Shifts.LOUNGE.value: 0.5
     }
 
-    return break_duration.get(shift_name)
+    return break_duration.get(shift_name, 0.5)
